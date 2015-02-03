3c4bdc309dea36f969afd195f83827b540ad70a9
Runjia <runjia.chen44@gmail.com>
2015-02-03 15:15:34 -0800

    Original Fractal

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,6 +1,22 @@
-private double divideAngle = 2.1;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
+private double divideAngle = 2.1f;
 private double smallestEllipse= 30;
-private double fractionLength = .8;
+private double fractionLength = .8f;
 
 public void setup(){
 	size(800,800);
@@ -71,4 +87,13 @@ public void drawEllipse2(int x,int y,double radius){
 		drawEllipse2(newX3,newY3,radius);
 		drawEllipse2(newX4,newY4,radius);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
